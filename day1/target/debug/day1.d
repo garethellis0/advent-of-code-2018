0ccbc3e48d826e6ea0a7007b6ba539e7a7089a41
Gareth Ellis <gareth.ellis0@gmail.com>
2018-12-01 20:05:48 -0800

    Removed BUild Artifacts

--- day1/target/debug/day1.d
+++
@@ -1,1 +0,0 @@
-/home/gareth/programming/advent-of-code-2018/day1/target/debug/day1: /home/gareth/programming/advent-of-code-2018/day1/src/main.rs
--- day1/target/debug/deps/day1-95513ff3df129aa9.d
+++
@@ -1,5 +0,0 @@
-/home/gareth/programming/advent-of-code-2018/day1/target/debug/deps/day1-95513ff3df129aa9: src/main.rs
-
-/home/gareth/programming/advent-of-code-2018/day1/target/debug/deps/day1-95513ff3df129aa9.d: src/main.rs
-
-src/main.rs:
